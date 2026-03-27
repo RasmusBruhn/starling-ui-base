f0fea1e136a35090d5bc9e39dd9fc6b5454f97d3
RasmusBruhn <rasmusbruhnnielsen@gmail.com>
2026-03-27 17:12:10 +0100

    Added constructors and getters for the physical widget geometry

--- src/geometry/physical.rs
+++ src/geometry/physical.rs
@@ -8,3 +8,44 @@ pub struct WidgetPhysicalGeometry<T: Coord> {
     /// The position and size of the widget relative to its root viewport
     absolute: Box<T>,
 }
+
+impl<T: Coord> WidgetPhysicalGeometry<T> {
+    /// Constructs a new physical widget geometry
+    ///
+    /// # Parameters
+    ///
+    /// relative: The position and size of the widget relative to its parent viewport
+    ///
+    /// absolute: The position and size of the widget relative to its root viewport
+    pub fn new(relative: Box<T>, absolute: Box<T>) -> Self {
+        return Self { relative, absolute };
+    }
+
+    /// Constructs a new physical widget geometry using the absolute geometry of
+    /// its parent viewport
+    ///
+    /// # Parameters
+    ///
+    /// relative: The position and size of the widget relative to its parent viewport
+    ///
+    /// parent: The position and size of the parent widget relative to its parent viewport
+    pub fn from_parent(relative: Box<T>, parent: &Box<T>) -> Self {
+        let parent_size = parent.get_size();
+        let size = relative.get_size() * parent_size;
+        let ll = relative.ll * parent_size + parent.ll;
+        let ur = ll + size;
+        let absolute = Box::new(ll, ur);
+
+        return Self::new(relative, absolute);
+    }
+
+    /// Retrieves the position and size of the widget relative to its parent viewport
+    pub fn get_relative(&self) -> &Box<T> {
+        return &self.relative;
+    }
+
+    /// Retrieves the position and size of the widget relative to its root viewport
+    pub fn get_absolute(&self) -> &Box<T> {
+        return &self.absolute;
+    }
+}
